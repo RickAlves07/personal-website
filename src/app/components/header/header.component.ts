bb31f16295003d93adb666dedb99de26b708abdd
Rick Alves <rick_07@hotmail.com.br>
2021-06-13 21:14:57 -0300

    configuração do header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit
+{
+	public navMenu: any;
+	public navToggle: any;
+	public navClose: any;
+	public navLinkItems: any
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit()
+	{
+		this.initHeaderComponents()
+	}
 
+	initHeaderComponents()
+	{
+		this.navMenu = document.getElementById('nav-menu');
+		this.navToggle = document.getElementById('nav-toggle');
+		this.navClose = document.getElementById('nav-close');
+		this.navLinkItems = document.querySelectorAll('.nav-link');
+	}
+
+	navToggleShowMenu()
+	{
+		this.navMenu.classList.add('show-menu');
+	}
+
+	navToggleCloseMenu()
+	{
+		this.navMenu.classList.remove('show-menu');
+	}
+
+	navLinkClick()
+	{
+		this.navToggleCloseMenu()
+	}
 }
